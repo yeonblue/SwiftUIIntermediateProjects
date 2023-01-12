c4738c67896cc4f375e3e0fdde1d0ecbe02df37f
yeonblue <yeondublue@naver.com>
2023-01-12 15:10:08 +0900

    add viewBilder

---
+++ Advanced/Advanced/9. ViewBuilderPractice.swift
@@ -0,0 +1,20 @@
+//
+//  ViewBuilderPractice.swift
+//  Advanced
+//
+//  Created by yeonBlue on 2023/01/12.
+//
+
+import SwiftUI
+
+struct ViewBuilderPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewBuilderPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewBuilderPractice()
+    }
+}
