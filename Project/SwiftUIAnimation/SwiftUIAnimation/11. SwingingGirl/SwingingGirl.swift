70971ac1d0819ea8c9c088a3c867d6c1637dde87
yeonblue <yeondublue@naver.com>
2023-03-02 11:02:41 +0900

    add swingingGirl

--- Project/SwiftUIAnimation/SwiftUIAnimation/11. SwingingGirl/SwingingGirl.swift
+++ Project/SwiftUIAnimation/SwiftUIAnimation/11. SwingingGirl/SwingingGirl.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct SwingingGirl: View {
+    
+    @State private var girl = false
+    @State private var leftLeg = false
+    @State private var rightLeg = false
+    
+    let fadeOutRope = Gradient(colors: [.clear, .black])
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("tree")
+                .resizable()
+                .frame(width: 550, height: 940)
+            
+            ZStack {
+                
+                // left leg
+                Image("leg")
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(.degrees(leftLeg ? -45 : 30), anchor: .topTrailing)
+                    .scaleEffect(0.12)
+                    .offset(x: -455, y: 90)
+                    .animation(.easeInOut(duration: 0.5).delay(1).repeatForever(autoreverses: true), value: leftLeg)
+                    .zIndex(1)
+                
+                // right leg
+                Image("leg")
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(.degrees(rightLeg ? -20 : 50), anchor: .topTrailing)
+                    .scaleEffect(0.12)
+                    .offset(x: -448, y: 90)
+                    .animation(.easeInOut(duration: 1).delay(0.3).repeatForever(autoreverses: true), value: rightLeg)
+                
+                // girl
+                Image("girl")
+                    .resizable()
+                    .scaledToFit()
+                    .scaleEffect(0.25)
+                    .offset(x: -300, y: 0)
+                
+                // rope
+                Image("rope")
+                    .resizable()
+                    .scaledToFit()
+                    .mask(LinearGradient(gradient: fadeOutRope, startPoint: .top, endPoint: .bottom))
+                    .frame(width: 57, height: 80)
+                    .offset(x: -228, y: -108)
+                
+                Image("rope")
+                    .resizable()
+                    .scaledToFit()
+                    .mask(LinearGradient(gradient: fadeOutRope, startPoint: .top, endPoint: .bottom))
+                    .frame(width: 57, height: 80)
+                    .offset(x: -189, y: -100)
+                
+            }
+            .rotationEffect(.degrees(girl ? -20 : -45), anchor: .top)
+            .animation(.easeInOut(duration: 1).delay(0.3).repeatForever(autoreverses: true), value: girl)
+            
+            Image("leaves")
+                .resizable()
+                .scaledToFit()
+                .rotationEffect(.degrees(-15), anchor: .trailing)
+                .frame(width: 400, height: 400)
+                .offset(x: -10, y: -180)
+        }
+        .onAppear {
+            leftLeg.toggle()
+            rightLeg.toggle()
+            girl.toggle()
+        }
+        
+        .frame(width: 950, height: 900)
     }
 }
 
